ad9a1903c657feee0b66bce507a29faff6553550
ZeNZeRo <zenzero1104@gmail.com>
2025-11-24 15:48:09 +0000

    C programming basics

---
+++ Basic/1.3/1.3.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+int main(){
+    int  num1,num2;
+    int operationCode;
+    int result;
+
+    
+  
+    printf("Enter num1 :");
+    scanf("%d", &num1);
+
+    printf("Enter num2 :");
+    scanf("%d", &num2);
+
+    printf("Enter operation code (1:+ 2:- 3:* 4:/): ");
+    scanf("%d", &operationCode);
+
+       switch (operationCode) {
+        case 1: 
+
+            result = num1 + num2;
+            printf("Result = %d\n", result);
+            break;
+
+        case 2: 
+
+            result = num1 - num2;
+            printf("Result = %d\n", result);
+            break;
+
+        case 3: 
+
+            result = num1 * num2;
+            printf("Result = %d\n", result);
+            break;
+
+        case 4: 
+
+            if (num2 == 0) {
+                printf("Cannot divide by zero\n");
+            } 
+            else {
+                result = num1 / num2; 
+                printf("Result = %d\n", result);
+            }
+
+            break;
+
+        default:
+
+            printf("Invalid Operation\n");
+            break;
+
+    }
+
+
+    return 0;
+
+
+}
+    
+
