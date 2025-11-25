cf42b0610caf4b63263144a67f27e042fb4f03ea
ZeNZeRo <zenzero1104@gmail.com>
2025-11-25 08:21:52 +0000

    C programming basics

--- Basic/1.10/1.10.c
+++ Basic/1.10/1.10.c
@@ -1,33 +1,29 @@
 #include <stdio.h>
 
 int main() {
-    int clearanceLevel, age , isActive;
+    int clearanceLevel, age, isActive;
 
-
-    printf("Enter your ClearanceLevel : ");
+    printf("Enter your Clearance Level : ");
     scanf("%d", &clearanceLevel);
 
-    printf("Enter your age : ");
+    printf("Enter your Age : ");
     scanf("%d", &age);
-    
-    printf("Enter your Active : ");
+
+    printf("Enter your Active (1=Yes, 0=No) : ");
     scanf("%d", &isActive);
 
-    if(clearanceLevel == 3 && isActive == 1 ){
-
+    if (clearanceLevel == 3 && isActive == 1) {
         printf("Access Granted\n");
-
     }
-    else if(clearanceLevel == 2 && age >=25 && isActive == 1){
-
-        printf("Access Granted");
-
+    else if (clearanceLevel == 2 && age >= 25 && isActive == 1) {
+        printf("Access Granted\n");
     }
-    else{
-
-        printf("Access Denied");
-
+    else {
+        printf("Access Denied\n");
     }
+
+
+    return 0;
 
 
 }
