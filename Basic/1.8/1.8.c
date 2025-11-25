7050e0ac83999d26f49ae1932c734b30b4a733ac
ZeNZeRo <zenzero1104@gmail.com>
2025-11-25 06:18:52 +0000

    C programming basics

--- Basic/1.8/1.8.c
+++ Basic/1.8/1.8.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 int main() {
+
     int dayCode, hour;
 
-    
-    if (scanf("%d %d", &dayCode, &hour) != 2) {
+
+    if (scanf("%d %d", &dayCode, &hour) != 2)  {
         return 1;
+        
     }
 
     switch (dayCode) {
