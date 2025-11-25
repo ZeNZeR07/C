3d76317d374152b5a2a2b331132c489dfdb362cf
ZeNZeRo <zenzero1104@gmail.com>
2025-11-25 15:56:13 +0000

    C programming basics

--- Basic/2.3/2.3.c
+++ Basic/2.3/2.3.c
@@ -7,15 +7,17 @@ int main() {
     float aprRate = 0.0;
     float totalInterest = 0.0;
 
-    printf("Enter Term Code (1, 2, 3): ");
+
+    printf("Enter Term Code (1, 2, 3) : ");
     scanf("%d", &termCode);
 
     printf("Enter Investment Amount: ");
     scanf("%f", &investmentAmount);
 
-    switch (termCode) {
 
+    switch ( termCode ) {
         case 1:
+
             if (investmentAmount < 5000) {
                 aprRate = 3.0;
             } else {
@@ -24,24 +26,34 @@ int main() {
             break;
 
         case 2:
+
             if (investmentAmount < 10000) {
+
                 aprRate = 5.0;
+
             } else {
+
                 aprRate = 6.5;
+
             }
             break;
 
         case 3:
             aprRate = 8.0;
+
             break;
 
         default:
+
             printf("Invalid Term Code\n");
             return 0;
     }
 
+
     totalInterest = investmentAmount * aprRate / 100;
-    printf("Total Interest: %.2f\n", totalInterest);
+    printf("Total Interest :  %.2f\n", totalInterest);
 
     return 0;
+
+
 }
