543a87a62bc59529ba5b74278c01d7aa9d2a70d6
ZeNZeRo <zenzero1104@gmail.com>
2025-11-25 07:10:10 +0000

    C programming basics

---
+++ Basic/1.9/1.9.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main() {
+    int zoneCode ;
+    float weight_kg ;
+    float totalCost = 0.0 ;
+
+
+    printf("Enter Zone Code (1=Local, 2=Regional, 3=International): ");
+    scanf("%d", &zoneCode);
+
+    printf("Enter Weight (kg): ");
+    scanf("%f", &weight_kg);
+
+    switch (zoneCode) {
+
+        case 1: 
+            if (weight_kg <= 5.0) {
+                totalCost = 50.0;
+            } else {
+                totalCost = 80.0;
+            }
+            break;
+
+
+        case 2: 
+            if (weight_kg <= 10.0) {
+                totalCost = 150.0;
+            } else {
+                totalCost = 250.0;
+            }
+            break;
+
+        case 3: 
+            totalCost = 500.0;
+            break;
+
+        default:
+
+            printf("Invalid Zone Code\n");
+            return 0;
+
+
+    }
+
+
+    printf("Total Cost: %.2f Baht\n", totalCost);
+
+    return 0 ;
+
+
+}
