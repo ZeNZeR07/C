d1a9efdc668107bc4fce1961e90bbd6824649def
ZeNZeRo <zenzero1104@gmail.com>
2025-11-25 13:25:14 +0000

    C programming basics

--- Basic/1.12/1.12.c
+++ Basic/1.12/1.12.c
@@ -11,39 +11,31 @@ int main() {
     printf("Enter Data Usage in GB: ");
     scanf("%f", &dataUsage_GB);
 
-    if (planCode == 1) {   
+
+    if (planCode == 1) {       
+            
         totalBill = 299.0;
 
         if (dataUsage_GB > 10.0) {
-
             totalBill += 10.0 * (dataUsage_GB - 10.0);
-
         }
     }
-    
-    else if (planCode == 2) {   
+    else if (planCode == 2) {    
+
         totalBill = 599.0;
 
-        if (dataUsage_GB > 15.0 && dataUsage_GB <= 20.0) {
-
-            totalBill += 50.0;
-
-        }
-        else if (dataUsage_GB > 20.0) {
-
+        if (dataUsage_GB > 20.0) {
             totalBill += 50.0 + 5.0 * (dataUsage_GB - 20.0);
-            
         }
     }
-
     else {
         printf("Invalid Plan Code\n");
         return 0;
     }
 
+
     printf("Total Bill: %.2f\n", totalBill);
 
     return 0;
-
 
 }
