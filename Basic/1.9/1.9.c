0c38f39f3a127668315afde0329f44a6855575bc
ZeNZeRo <zenzero1104@gmail.com>
2025-11-25 07:11:37 +0000

    C programming basics

--- Basic/1.9/1.9.c
+++ Basic/1.9/1.9.c
@@ -15,8 +15,10 @@ int main() {
     switch (zoneCode) {
 
         case 1: 
+
             if (weight_kg <= 5.0) {
                 totalCost = 50.0;
+
             } else {
                 totalCost = 80.0;
             }
@@ -24,14 +26,17 @@ int main() {
 
 
         case 2: 
+
             if (weight_kg <= 10.0) {
                 totalCost = 150.0;
+
             } else {
                 totalCost = 250.0;
             }
             break;
 
         case 3: 
+        
             totalCost = 500.0;
             break;
 
