f71108fcd2a482617f6a7c3ad53e60abd3ef08b4
ZeNZeRo <zenzero1104@gmail.com>
2025-11-25 13:15:02 +0000

    C programming basics

--- Basic/1.12/1.12.c
+++ Basic/1.12/1.12.c
@@ -3,47 +3,47 @@
 int main() {
     int planCode;
     float dataUsage_GB;
-    
+    float totalBill = 0.0;
 
-    printf("Enter Your Plan Code (1.Basic, 2.Premium): ");
+    printf("Enter Your Plan Code (1 = Basic, 2 = Premium): ");
     scanf("%d", &planCode);
 
     printf("Enter Data Usage in GB: ");
     scanf("%f", &dataUsage_GB);
 
-   
-    if(planCode == 1) {  
+    if (planCode == 1) {   
+        totalBill = 299.0;
 
-        float totalbill = 299.0;
-        if(dataUsage_GB > 10.0) {
-            totalbill += 10*(dataUsage_GB - 10.0);
+        if (dataUsage_GB > 10.0) {
+
+            totalBill += 10.0 * (dataUsage_GB - 10.0);
+
         }
-
-        printf("Total Bill: %.2f\n", totalbill);
-
-
     }
-
-    else if (planCode == 2) {          
-
-        float totalbill = 599.0;
-        if(dataUsage_GB > 15.0) {
-
-            totalbill += 50 + 5*(dataUsage_GB - 20.0);
-        }
-
-        printf("Total Bill: %.2f\n", totalbill);
-
-
-    }
-   
-    else {
-
-        printf("Invalid Plan Code\n");
-        
-        return 0;
-
-    }
-
     
+    else if (planCode == 2) {   
+        totalBill = 599.0;
+
+        if (dataUsage_GB > 15.0 && dataUsage_GB <= 20.0) {
+
+            totalBill += 50.0;
+
+        }
+        else if (dataUsage_GB > 20.0) {
+
+            totalBill += 50.0 + 5.0 * (dataUsage_GB - 20.0);
+            
+        }
+    }
+
+    else {
+        printf("Invalid Plan Code\n");
+        return 0;
+    }
+
+    printf("Total Bill: %.2f\n", totalBill);
+
+    return 0;
+
+
 }
