359c266667d3c1790a376b46f1978355036faad0
ZeNZeRo <zenzero1104@gmail.com>
2025-11-25 16:00:46 +0000

    C programming basics

--- Basic/2.5/2.5.c
+++ Basic/2.5/2.5.c
@@ -1,28 +1,22 @@
 #include <stdio.h>
 
-int main(){
-    int score;
-    int countA = 0 , countB = 0, countC = 0, countD = 0, countF = 0 ;
+int main() {
+    int score = 0;
+    int countA = 0, countB = 0, countC = 0, countD = 0, countF = 0;
 
-
-    while (score != -1){
-
-        printf("Enter score: ");
+    while (score != -1) {
+        printf("Enter score (-1 to stop): ");
         scanf("%d", &score);
 
-        if(score >= 80 && score <= 100){
+        if (score >= 80 && score <= 100) {
             countA++;
-        }
-        else if(score >= 70){
+        } else if (score >= 70 && score <= 79) {
             countB++;
-        }
-        else if(score >= 60){
+        } else if (score >= 60 && score <= 69) {
             countC++;
-        }
-        else if(score >= 50){
-            countD++;   
-        }
-        else if(score >= 0){
+        } else if (score >= 50 && score <= 59) {
+            countD++;
+        } else if (score >= 0 && score <= 49) {
             countF++;
         }
        
@@ -34,8 +28,5 @@ int main(){
     printf("Grade D Count: %d\n", countD);
     printf("Grade F Count: %d\n", countF);
 
-
     return 0;
-
-
 }
