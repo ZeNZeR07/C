c77dc2131d3d43d2881d953ce98752fa328db219
ZeNZeRo <zenzero1104@gmail.com>
2025-11-25 05:12:32 +0000

    C programming basics

--- Basic/1.4/1.4.c
+++ Basic/1.4/1.4.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
 int main(){
-    int N, sum = 0;
+    int N, sum = 0 ;
 
     printf("Enter your number:");
     scanf("%d", &N);
 
-    for(int i = 1; i <= N; i++){
-        sum += i;
+    for( int i = 1 ;  i <= N ;  i++ ){
+        sum += i ;
 
         
     }
 
-
     printf("%d\n", sum);
+
+
     return 0;
 
 
