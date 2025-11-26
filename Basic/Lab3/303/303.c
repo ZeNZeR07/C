86a4bba857867493a4907265ee76bb3169b53565
ZeNZeRo <zenzero1104@gmail.com>
2025-11-26 15:49:37 +0000

    C programming basics

--- Basic/Lab3/303/303.c
+++ Basic/Lab3/303/303.c
@@ -1,36 +1,32 @@
 #include <stdio.h>
 
-struct Student{
+struct Student {
     char name[50];
     int studentId;
     float score;
-    
-    
-}; 
+};
 
 int main() {
-    int N, i ;
-    float totalScore, AverageScore;
+    int N, i;
+    float totalScore = 0.0f, averageScore;
 
-    if(scanf("%d", &N) != 1 || N <= 0){
+    if (scanf("%d", &N) != 1 || N <= 0) {
         return 1;
     }
- 
-    struct Student students[N];
-    
-    for(i = 0; i < N; i++){
 
-        if(scanf("%d %f %s", &students[i].studentId, &students[i].score, students[i].name) != 3){
+    struct Student students[N];
+
+    for (i = 0; i < N; i++) {
+        if (scanf("%d %f %s", &students[i].studentId, &students[i].score, students[i].name) != 3) {
             return 1;
         }
+
         totalScore += students[i].score;
-
     }
-    AverageScore = totalScore / N;
 
-    printf("Average Score: %.2f", AverageScore);
+    averageScore = totalScore / N;
+
+    printf("Average Score: %.2f\n", averageScore);
 
     return 0;
-
-
 }
