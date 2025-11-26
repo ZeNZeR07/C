1aef782aeb3f150d632954fc609430544247f690
ZeNZeRo <zenzero1104@gmail.com>
2025-11-26 14:32:55 +0000

    C programming basics

--- Basic/Lab3/304/3.4.c
+++ Basic/Lab3/304/3.4.c
@@ -1,43 +1,12 @@
 #include <stdio.h>
 
-struct Student{
-    char name[50];
-    int studentId;
-    float score;
+struct Order{
+    
+    
 };
 
 int main() {
-    int N, i;
-    int passedCount = 0;
-    int failedCount = 0;
-
-    if(scanf("%d", &N) != 1 || N <= 0){
-        return 1;
-    }
- 
-    struct Student students[N];
-    
-    for(i = 0; i < N; i++){
-
-        if(scanf("%d %f %s", &students[i].studentId, &students[i].score, students[i].name) != 3){
-            return 1;
-        }
-
-
-    }
-
-    for(i = 0; i < N; i++){
-        if(students[i].score >= 60.0){
-            passedCount++;
-        } else {
-            failedCount++;
-        }
-    }
-
-    printf("Passed Count: %d\n", passedCount);
-    printf("Failed Count: %d\n", failedCount);
-
-    return 0;
+   
 
 
 }
