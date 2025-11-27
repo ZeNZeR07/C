1e7885add2d34c18ac865e776b4bf073da6cd69f
ZeNZeRo <zenzero1104@gmail.com>
2025-11-27 12:34:36 +0000

    C programming basics

--- Basic/Lab3/307/3.7.c
+++ Basic/Lab3/307/3.7.c
@@ -1,40 +1,53 @@
 #include <stdio.h>
 
 struct SalesRecord {
-    char name[50];
+    char  name[50];
     float target;
     float actual;
 };
 
 int main(void) {
-    int N, i;
+    int   N = 0;
+    int   i;
     float grandTotalCommission = 0.0f;
 
+
     if (scanf("%d", &N) != 1 || N <= 0) {
+        printf("Invalid number of records.\n");
         return 1;
     }
 
     struct SalesRecord records[N];
 
     for (i = 0; i < N; i++) {
-        float baseCommission;
+        float baseCommission = 0.0f;
 
-    
-        if (scanf("%f %f %s", &records[i].target, &records[i].actual, records[i].name) != 3) {
+
+        if (scanf("%f %f %49s",
+                  &records[i].target,
+                  &records[i].actual,
+                  records[i].name) != 3) {
+            printf("Invalid input data.\n");
             return 1;
         }
 
-        baseCommission = records[i].actual * 0.10;
 
-        if (records[i].actual >= records[i].target * 1.20) {
-            baseCommission += 200.0;
-        } 
-        else if (records[i].actual >= records[i].target) {
-            baseCommission += 50.0;
-        } 
-        else if (records[i].actual < records[i].target * 0.90) {
-            baseCommission -= 100.0;
+        if (records[i].target <= 0.0f || records[i].actual < 0.0f) {
+            printf("Invalid target/actual for %s. Skipping.\n", records[i].name);
+            continue;
         }
+
+    
+        baseCommission = records[i].actual * 0.10f;
+
+       
+        if (records[i].actual > records[i].target * 1.20f) {     
+            baseCommission += 200.0f;
+        } else if (records[i].actual >= records[i].target) {     
+            baseCommission += 50.0f;
+        } else if (records[i].actual < records[i].target * 0.90f) { 
+            baseCommission -= 100.0f;
+        }  
 
         grandTotalCommission += baseCommission;
     }
