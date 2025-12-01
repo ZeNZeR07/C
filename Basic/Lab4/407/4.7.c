2ed18daacd94c335376775c35bf00ad54c58a107
ZeNZeRo <zenzero1104@gmail.com>
2025-12-01 08:23:39 +0000

    C programming basics

--- Basic/Lab4/407/4.7.c
+++ Basic/Lab4/407/4.7.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define MAX_SIZE 10
+#define MAX_SIZE 10  
 
 int main() {
     int data[MAX_SIZE];
@@ -10,22 +10,36 @@ int main() {
     int i;
 
     printf("Enter the number of elements (N, max %d): ", MAX_SIZE);
-    scanf("%d", &N);
+
+    if (scanf("%d", &N) != 1) {
+      
+        return 1;
+    }
 
     if (N > MAX_SIZE || N < 1) {
+      
         N = MAX_SIZE;
     }
 
     printf("Enter %d integer numbers:\n", N);
+
     for (i = 0; i < N; i++) {
         printf("Element %d: ", i + 1);
-        scanf("%d", &data[i]);
+
+        if (scanf("%d", &data[i]) != 1) {
+            
+            return 1;
+        }
     }
 
     printf("Enter the search value: ");
-    scanf("%d", &search_value);
+
+    if (scanf("%d", &search_value) != 1) {
+        return 1;
+    }
 
     for (i = 0; i < N; i++) {
+       
         if (data[i] == search_value) {
             count++;
         }
@@ -35,9 +49,11 @@ int main() {
     printf("Total elements recorded (N): %d\n", N);
 
     printf("Recorded Numbers: ");
+
     for (i = 0; i < N; i++) {
         printf("%d ", data[i]);
     }
+
     printf("\n");
 
     printf("Search Value: %d\n", search_value);
