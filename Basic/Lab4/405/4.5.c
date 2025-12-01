8f6a608ad5ae81e500afee1c426cf7cc56f894dc
ZeNZeRo <zenzero1104@gmail.com>
2025-12-01 05:43:57 +0000

    C programming basics

--- Basic/Lab4/405/4.5.c
+++ Basic/Lab4/405/4.5.c
@@ -2,30 +2,22 @@
 
 #define SIZE 6
 
-int main() {
+int main(void) {
     int data[SIZE];
     int total_sum = 0;
     int i;
 
-
     for (i = 0; i < SIZE; i++) {
-        printf("Enter integer number %d: ", i + 1);
         scanf("%d", &data[i]);
-    }
-
-
-    for (i = 0; i < SIZE; i++) {
         total_sum += data[i];
     }
 
-  
-    printf("\n--- SUMMATION REPORT ---\n");
+    printf("--- SUMMATION REPORT ---\n");
     printf("Recorded Numbers: ");
     for (i = 0; i < SIZE; i++) {
         printf("%d ", data[i]);
     }
     printf("\n");
-
     printf("Total Sum of Numbers: %d\n", total_sum);
 
     return 0;
