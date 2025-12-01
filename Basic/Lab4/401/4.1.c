209126018e415ff60851e1c407eb49b39c164806
ZeNZeRo <zenzero1104@gmail.com>
2025-12-01 08:07:00 +0000

    C programming basics

--- Basic/Lab4/401/4.1.c
+++ Basic/Lab4/401/4.1.c
@@ -4,18 +4,22 @@ int main() {
     int N;
     int i;
 
+
     if (scanf("%d", &N) != 1) {
         return 1;
     }
 
     int scores[N];
 
+
     for (i = 0; i < N; i++) {
+
         if (scanf("%d", &scores[i]) != 1) {
             return 1;
         }
     }
 
+   
     for (i = N - 1; i >= 0; i--) {
         printf("%d\n", scores[i]);
     }
