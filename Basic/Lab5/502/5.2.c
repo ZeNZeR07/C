abc56af7e97b6db490cc302330c2a44b30ca5a42
ZeNZeRo <zenzero1104@gmail.com>
2025-12-01 14:09:54 +0000

    C programming basics

---
+++ Basic/Lab5/502/5.2.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+struct Employee {
+    int id;
+    int gross_salary;
+    int bonus;
+    int total_income;
+};
+
+int calculate_net_salary(int total_income);
+
+int main(void) {
+    struct Employee emp;
+    int net_salary;
+
+    printf("Enter Employee ID: ");
+    if (scanf("%d", &emp.id) != 1) {
+        return 1;
+    }
+
+    printf("Enter Gross Salary: ");
+    if (scanf("%d", &emp.gross_salary) != 1) {
+        return 1;
+    }
+
+    printf("Enter Bonus: ");
+    if (scanf("%d", &emp.bonus) != 1) {
+        return 1;
+    }
+
+    emp.total_income = emp.gross_salary + emp.bonus;
+    net_salary = calculate_net_salary(emp.total_income);
+
+    printf("\n--- EMPLOYEE SALARY REPORT ---\n");
+    printf("Employee ID: %d\n", emp.id);
+    printf("Gross Salary: %d\n", emp.gross_salary);
+    printf("Bonus: %d\n", emp.bonus);
+    printf("Total Income: %d\n", emp.total_income);
+    printf("Net Salary: %d\n", net_salary);
+
+    return 0;
+}
+
+int calculate_net_salary(int total_income) {
+    int tax;
+
+    if (total_income > 30000) {
+        tax = total_income * 10 / 100;
+    } else {
+        tax = total_income * 5 / 100;
+    }
+
+    return total_income - tax;
+}
