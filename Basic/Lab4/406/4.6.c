9b673c6a1567a5ba6f41975a2b4dbf7125862eb9
ZeNZeRo <zenzero1104@gmail.com>
2025-12-01 08:18:48 +0000

    C programming basics

--- Basic/Lab4/406/4.6.c
+++ Basic/Lab4/406/4.6.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 
-#define STUDENTS 5
+#define STUDENTS 5  
 
-int main() {
-    int scores[STUDENTS];
-    int sum = 0;
-    float average;
-    int above_average_count = 0;
+int main(void) {
+    int scores[STUDENTS];     
+    int sum = 0;             
+    float average;           
+    int above_average_count = 0; 
     int i;
 
+
     for (i = 0; i < STUDENTS; i++) {
-        scanf("%d", &scores[i]);
+        if (scanf("%d", &scores[i]) != 1) {
+            return 1;
+        }
     }
+
 
     for (i = 0; i < STUDENTS; i++) {
         sum += scores[i];
     }
 
+
     average = (float)sum / STUDENTS;
+
 
     for (i = 0; i < STUDENTS; i++) {
         if (scores[i] > average) {
