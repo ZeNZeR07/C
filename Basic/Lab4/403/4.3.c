ecb1b4cfddf0284d9a6df88d89a192ba41abcaca
ZeNZeRo <zenzero1104@gmail.com>
2025-12-01 04:28:47 +0000

    C programming basics

--- Basic/Lab4/403/4.3.c
+++ Basic/Lab4/403/4.3.c
@@ -2,30 +2,25 @@
 
 #define DAYS 7
 
-int main() {
+int main(void) {
     int daily_temp[DAYS];
     int max_temp;
     int i;
 
     for (i = 0; i < DAYS; i++) {
         scanf("%d", &daily_temp[i]);
-    }
-
-    max_temp = daily_temp[0];
-
-    for (i = 1; i < DAYS; i++) {
-        if (daily_temp[i] > max_temp) {
+        
+        if (i == 0 || daily_temp[i] > max_temp) {
             max_temp = daily_temp[i];
         }
     }
 
+
     printf("\n--- DAILY TEMPERATURE REPORT ---\n");
     printf("Recorded Temperatures (C): ");
-
     for (i = 0; i < DAYS; i++) {
         printf("%d ", daily_temp[i]);
     }
-
     printf("\n");
     printf("Maximum Temperature Found: %d C\n", max_temp);
 
