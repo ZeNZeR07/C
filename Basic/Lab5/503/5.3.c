571c595479ebd4b7f4c5b5a2b2e06fad2ea9e83a
ZeNZeRo <zenzero1104@gmail.com>
2025-12-02 07:19:04 +0000

    C programming basics

--- Basic/Lab5/503/5.3.c
+++ Basic/Lab5/503/5.3.c
@@ -2,27 +2,56 @@
 
 #define SIZE 5
 
-int calculate_sum(int arr[], int size){
+int calculate_sum(int arr[], int size) {
+
+    int i;
+    int sum = 0;
+
+    for (i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+
+    return sum;
+
 
 }
 
-int main(){
+int main(void) {
+
     int numbers[SIZE];
     int total_sum;
     float average;
     int i;
 
+   
 
-    printf("Enter %d interger numbers:\n", SIZE);
-    for(i = 0; i < SIZE; i++){
-        if(scanf("%d", &numbers[i]) != 1){
+    for (i = 0; i < SIZE; i++) {
+
+        printf("Enter %d integer numbers: ", SIZE);
+
+        if (scanf("%d", &numbers[i]) != 1) {
             return 1;
+
         }
+
     }
 
-
+    total_sum = calculate_sum(numbers, SIZE);
     average = (float)total_sum / SIZE;
 
     printf("\n--- ARRAY STATISTICS REPORT ---\n");
+    printf("Recorded Numbers: ");
 
+    for (i = 0; i < SIZE; i++) {
+
+        printf("%d ", numbers[i]);
+
+        
+    }
+
+    printf("\n");
+    printf("Total Sum: %d\n", total_sum);
+    printf("Average: %.2f\n", average);
+
+    return 0;
 }
