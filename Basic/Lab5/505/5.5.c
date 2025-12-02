8acece86a891d0ccea23c3003d84fa39f4c69df0
ZeNZeRo <zenzero1104@gmail.com>
2025-12-02 12:42:11 +0000

    C programming basics

--- Basic/Lab5/505/5.5.c
+++ Basic/Lab5/505/5.5.c
@@ -6,14 +6,18 @@ struct Student {
     int id;
 };
 
+
 const char* check_id_existence(struct Student students[], int size, int search_id) {
     int i;
 
     for (i = 0; i < size; i++) {
+
+
         if (students[i].id == search_id) {
             return "ID FOUND in the system.";
         }
     }
+
 
     return "ID NOT FOUND in the system.";
 }
@@ -22,22 +26,30 @@ int main(void) {
     struct Student id_list[NUM_STUDENTS];
     int search_id;
     int i;
+    const char* result;
 
-    printf("Enter IDs for %d students \n", NUM_STUDENTS);
+    printf("Enter IDs for %d students\n", NUM_STUDENTS);
 
     for (i = 0; i < NUM_STUDENTS; i++) {
         printf("Student %d ID: ", i + 1);
-        scanf("%d", &id_list[i].id);
+
+        if (scanf("%d", &id_list[i].id) != 1) {
+            return 1;
+        }
     }
 
     printf("\nEnter ID to search: ");
-    scanf("%d", &search_id);
 
-    const char* result = check_id_existence(id_list, NUM_STUDENTS, search_id);
+    if (scanf("%d", &search_id) != 1) {
+        return 1;
+    }
+
+
+    result = check_id_existence(id_list, NUM_STUDENTS, search_id);
 
     printf("\n--- ID SEARCH REPORT ---\n");
-    printf("Recorded IDs: ");
 
+    printf("Recorded IDs: ");
     for (i = 0; i < NUM_STUDENTS; i++) {
         printf("%d ", id_list[i].id);
     }
