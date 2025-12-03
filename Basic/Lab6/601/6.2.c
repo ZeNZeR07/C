403b7f64905ea63ccf3aac805d9d4c880b9fb19e
ZeNZeRo <zenzero1104@gmail.com>
2025-12-03 07:36:47 +0000

    C programming basics

---
+++ Basic/Lab6/601/6.2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void increase_value(int *number_ptr);
+
+
+int main(){
+    int data;
+
+
+    printf("Enter initial integer value: ");
+    scanf("%d", &data);
+
+    printf("\n--- POINTER MODIFICATION REPORT ---\n");
+    printf("1. Value BEFORE function call : %d\n", data);
+
+    increase_value(&data);
+
+    return 0;
+
+}
+
+void increase_value(int *number_ptr){
+    *number_ptr = *number_ptr + 10;
+    printf("2. Value AFTER function call  : %d\n", *number_ptr);
+}
