4cd2c0ec70cbcbfb335f000f5b59fe777d52e95c
ZeNZeRo <zenzero1104@gmail.com>
2025-12-06 13:47:55 +0000

    C programming basics

--- Advanced/Lab1/103/1.3.c
+++ Advanced/Lab1/103/1.3.c
@@ -21,23 +21,30 @@ int *GetSet(int *numPtr) {
         return NULL;
     }
 
-    for (index = 0; index < count; index++) {
+    index = 0;
+    while (index < count) {
         printf("Enter data[%d] : ", index);
         scanStatus = scanf("%d", &arr[index]);
+
         if (scanStatus != 1) {
             count = index;
             break;
         }
+
+        index = index + 1;
     }
 
     *numPtr = count;
     return arr;
 }
 
-int main() {
+int main(void) {
     int *data;
     int num;
     int i;
+
+    data = NULL;
+    num = 0;
 
     data = GetSet(&num);
 
@@ -47,10 +54,15 @@ int main() {
     }
 
     printf("\n--- Result ---\n");
-    for (i = 0; i < num; i++) {
+
+    i = 0;
+    while (i < num) {
         printf("data[%d] = %d\n", i, data[i]);
+        i = i + 1;
     }
 
     free(data);
+    data = NULL;
+
     return 0;
 }
