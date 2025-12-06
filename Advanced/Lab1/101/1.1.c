50a29b1c58815b93b11ad0966eae956534e12bb9
ZeNZeRo <zenzero1104@gmail.com>
2025-12-06 12:41:42 +0000

    C programming Advanced

--- Advanced/Lab1/101/1.1.c
+++ Advanced/Lab1/101/1.1.c
@@ -1,29 +1,38 @@
 #include <stdio.h>
 
-void getN(int *pN) {
+void getLineCount(int *lineCountPtr) {
     printf("Enter N: ");
-    scanf("%d", pN); 
-}
 
-
-void printLine(int index) {
-    printf("[%d] Hello world\n", index);
-}
-
-
-void printUsingPointer(int n, void (*fp)(int)) {
-    for(int i = 1; i <= n; i++) {
-        fp(i);
+    if (scanf("%d", lineCountPtr) != 1) {
+        *lineCountPtr = 0;
     }
 }
 
-int main() {
-    int n;
+void printHelloLine(int lineIndex) {
+    printf("[%d] Hello world\n", lineIndex);
+}
 
-  
-    getN(&n);
 
-    printUsingPointer(n, printLine);
+void printUsingPointer(int lineCount, void (*printFunc)(int)) {
+    for (int currentLine = 1; currentLine <= lineCount; currentLine++) {
+        printFunc(currentLine);
+    }
+}
+
+int main(void) {
+    int lineCount = 0;
+
+ 
+    getLineCount(&lineCount);
+
+
+    if (lineCount <= 0) {
+        printf("Invalid N. Program will exit.\n");
+        return 1;
+    }
+
+
+    printUsingPointer(lineCount, printHelloLine);
 
     return 0;
 }
