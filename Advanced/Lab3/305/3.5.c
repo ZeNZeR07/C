b71dcca1a30a7b59c13bbc3407451d18212bfec2
ZeNZeRo <zenzero1104@gmail.com>
2025-12-22 14:08:02 +0000

    C programming basics

--- Advanced/Lab3/305/3.5.c
+++ Advanced/Lab3/305/3.5.c
@@ -1,50 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct student {
-    char name[ 20 ] ;
-    int age ;
-    char sex ;
-    float gpa ;
-} ;
+#define MAX_NAME_LENGTH 20
+#define STUDENTS_PER_ROOM 10
 
-struct student (*GetStudent( int *room ))[ 10 ] ;
+struct Student {
+    char name[MAX_NAME_LENGTH];
+    int age;
+    char sex;
+    float gpa;
+};
+
+typedef struct Student (*RoomArray)[STUDENTS_PER_ROOM];
+
+RoomArray allocate_and_fill_rooms(int *num_rooms);
+void print_student_info(RoomArray rooms, int num_rooms);
+void input_single_student(struct Student *s, int student_num);
 
 int main() {
-    struct student ( *children )[ 10 ] ;
-    int group ;
+    RoomArray classrooms;
+    int room_count = 0;
 
-    children = GetStudent( &group ) ;
+    classrooms = allocate_and_fill_rooms(&room_count);
 
-    for ( int i = 0 ; i < group ; i++ ) {
-        printf( "Room %d:\n", i + 1 ) ;
-        for ( int j = 0 ; j < 10 ; j++ ) {
-            printf( "Student %d: %s %d %c %.2f\n", 
-                    j + 1, 
-                    children[ i ][ j ].name, 
-                    children[ i ][ j ].age, 
-                    children[ i ][ j ].sex, 
-                    children[ i ][ j ].gpa ) ;
-        }
+    if (classrooms == NULL) {
+        return 1;
     }
-    return 0 ;
+
+    print_student_info(classrooms, room_count);
+
+    free(classrooms);
+
+    return 0;
 }
 
-struct student (*GetStudent( int *room ))[ 10 ] {
-    printf( "Enter number of rooms: " ) ;
-    scanf( "%d", room ) ;
+RoomArray allocate_and_fill_rooms(int *num_rooms) {
+    printf("Enter number of rooms: ");
+    if (scanf("%d", num_rooms) != 1) {
+        return NULL;
+    }
+
+    RoomArray rooms = malloc(*num_rooms * sizeof(struct Student[STUDENTS_PER_ROOM]));
     
-    struct student (*child)[ 10 ] = malloc( *room * sizeof( struct student[ 10 ] ) ) ;
-    
-    for ( int i = 0 ; i < *room ; i++ ) {
-        printf( "Room %d:\n", i + 1 ) ;
-        for ( int j = 0 ; j < 10 ; j++ ) {
-            printf( "Student %d (Name Age Sex GPA): ", j + 1 ) ;
-            scanf( "%s", child[ i ][ j ].name ) ;
-            scanf( "%d", &child[ i ][ j ].age ) ;
-            scanf( " %c", &child[ i ][ j ].sex ) ;
-            scanf( "%f", &child[ i ][ j ].gpa ) ;
+    if (rooms == NULL) {
+        printf("Memory allocation failed!\n");
+        return NULL;
+    }
+
+    for (int roomIdx = 0; roomIdx < *num_rooms; roomIdx++) {
+        printf("Room %d:\n", roomIdx + 1);
+        for (int studentIdx = 0; studentIdx < STUDENTS_PER_ROOM; studentIdx++) {
+            input_single_student(&rooms[roomIdx][studentIdx], studentIdx + 1);
         }
     }
-    return child ;
+    return rooms;
 }
+
+void input_single_student(struct Student *s, int student_num) {
+    printf("Student %d (Name Age Sex GPA): ", student_num);
+    scanf("%19s", s->name); 
+    scanf("%d", &s->age);
+    scanf(" %c", &s->sex);
+    scanf("%f", &s->gpa);
+}
+
+void print_student_info(RoomArray rooms, int num_rooms) {
+    for (int roomIdx = 0; roomIdx < num_rooms; roomIdx++) {
+        printf("Room %d:\n", roomIdx + 1);
+        for (int studentIdx = 0; studentIdx < STUDENTS_PER_ROOM; studentIdx++) {
+            printf("Student %d: %s %d %c %.2f\n", 
+                   studentIdx + 1, 
+                   rooms[roomIdx][studentIdx].name, 
+                   rooms[roomIdx][studentIdx].age, 
+                   rooms[roomIdx][studentIdx].sex, 
+                   rooms[roomIdx][studentIdx].gpa);
+        }
+    }
+}   
